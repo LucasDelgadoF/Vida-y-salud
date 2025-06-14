68a34166f921c286aaebbf2d7ac7bdc49c64c606
LucasDelgadoF <lu.delgadof@duocuc.cl>
2025-06-14 09:45:47 -0400

    Original
    Se define el diccionario afiliado y la estructura del menú.

---
+++ Vida_y_salud_clases.py
@@ -0,0 +1,26 @@
+from random import randint
+
+afiliados = {}
+
+while True:
+    print("\n---BIENVENIDO A ISAPRE MÁS VIDA---")
+    print("\n1. Grabar afiliado")
+    print("2. Buscar afiliado")
+    print("3. Imprimir certificado")
+    print("4. Salir")
+    opcion = input("Opción: ")
+
+    if opcion == "1":
+       
+    
+    elif opcion == "2":
+       
+    
+    elif opcion == "3":
+        
+    
+    elif opcion == "4":
+        
+
+    else:
+        print("Opción inválida")
