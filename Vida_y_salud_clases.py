7126934fa5c7d9ad2a92f283885974c3230ad708
dapintom <da.pintom@duocuc.cl>
2025-06-14 09:48:14 -0400

    Add files via upload
    En esta versión se añaden todas las opciones para almacenar los datos del afiliado en la lista rut y en el diccionario afiliado.

--- Vida_y_salud_clases.py
+++ Vida_y_salud_clases.py
@@ -11,7 +11,39 @@ while True:
     opcion = input("Opción: ")
 
     if opcion == "1":
-       
+        rut = input("RUT: ")
+        if rut in afiliados:
+            print("RUT ya registrado")
+            continue
+
+        nombre = input("Nombre: ")
+        apellido = input("Apellido paterno/materno: ")
+
+        try:
+            edad = int(input("Edad: "))
+            if edad < 18:
+                print("Edad debe ser mayor o igual a 18.")
+                continue
+        except:
+            print("Edad inválida. Debe ingresar un número.")
+            continue
+
+        estado_civil = input("Estado civil (C/S/V): ").upper()
+        genero = input("Género (M/F): ").upper()
+        fecha = input("Fecha afiliación (dd-mm-aaaa): ")
+
+        monto = randint(1000, 1500)
+
+        afiliados[rut] = {
+            "Nombre": nombre,
+            "Apellido": apellido,
+            "Edad": edad,
+            "Estado Civil": estado_civil,
+            "Género": genero,
+            "Fecha Afiliación": fecha,
+            "Monto Certificado": monto
+        }
+        print("Afiliado registrado")
     
     elif opcion == "2":
        
